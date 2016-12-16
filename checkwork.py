fa0340af250d87cabda7a6b1905f6d8059d4412c
vald Gonch <goncharov.vlad@gmail.com>
2016-12-16 14:02:22 +0700

    поправил задание контрольной

--- checkwork.py
+++ checkwork.py
@@ -49,7 +49,7 @@ def main():
         a = basedata.BaseAstrology(1973)
         m = a.get_months_of_year(1973)
         d = a.get_days_of_month(m[6])
-        h = a.get_hours_of_day(d[15])
+        h = a.get_hours_of_day(d[16])
         print(u"#7 признаки месяца:%s дня:%s часа:%s родившегося в год водяного быка перед рассветом" %
               (m[6][0]+' '+m[6][1], d[16][1]+' '+d[16][2], h[11][1]+' '+h[11][2]))
 
