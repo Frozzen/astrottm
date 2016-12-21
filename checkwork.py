c240e2d81188f8c8261cdec2e72f54454bb674f6
vald Gonch <goncharov.vlad@gmail.com>
2016-12-21 09:28:41 +0700

    refactor, проверка аргументов посчитал гороскоп для задания

--- checkwork.py
+++ checkwork.py
@@ -73,6 +73,44 @@ def main():
         pm = a.get_mevas(2016)['pawme']
         print(u"#доп жен: 1976гр на 2016 логмен:%s %s, парка:%s, павме:%s" % (l[0], l[1], p, pm))
 
+        a = basedata.BaseAstrology(1963)
+        m = a.get_mevas(2016)
+        print(u"#5 мева рожденого в 1963 на 2016: согме:%s люме:%s ванме:%s лунме:%s павме:%s "
+              % (m['sogme'], m['lume'], m['wangme'], m['lunme'], m['pawme']))
+        m = a.get_mevas(2017)
+        print(u"#5 мева рожденого в 1963 на 2017: согме:%s люме:%s ванме:%s лунме:%s павме:%s "
+              % (m['sogme'], m['lume'], m['wangme'], m['lunme'], m['pawme']))
+
+        a = basedata.BaseAstrology(1972)
+        m = a.get_mevas(2016)
+        print(u"#5 мева рожденого в 1972 на 2016: согме:%s люме:%s ванме:%s лунме:%s павме:%s "
+              % (m['sogme'], m['lume'], m['wangme'], m['lunme'], m['pawme']))
+        m = a.get_mevas(2017)
+        print(u"#5 мева рожденого в 1972 на 2017: согме:%s люме:%s ванме:%s лунме:%s павме:%s "
+              % (m['sogme'], m['lume'], m['wangme'], m['lunme'], m['pawme']))
+
+        a = basedata.BaseAstrology(2016)
+        m = a.get_mevas(2016)
+        print(u"#5 мева рожденого в 2016 на 2016: согме:%s люме:%s ванме:%s лунме:%s павме:%s "
+              % (m['sogme'], m['lume'], m['wangme'], m['lunme'], m['pawme']))
+
+        a = basedata.BaseAstrology(2017)
+        m = a.get_mevas(2017)
+        print(u"#5 мева рожденого в 2017 на 2017: согме:%s люме:%s ванме:%s лунме:%s павме:%s "
+              % (m['sogme'], m['lume'], m['wangme'], m['lunme'], m['pawme']))
+
+        a = basedata.BaseAstrology(1962)
+        m = a.get_mevas(2016)
+        print(u"#5 мева оли козыревой на 2016: согме:%s люме:%s ванме:%s лунме:%s павме:%s "
+              % (m['sogme'], m['lume'], m['wangme'], m['lunme'], m['pawme']))
+        m = a.get_mevas(2017)
+        print(u"#5 мева оли козыревой на 2017: согме:%s люме:%s ванме:%s лунме:%s павме:%s "
+              % (m['sogme'], m['lume'], m['wangme'], m['lunme'], m['pawme']))
+
+        a = basedata.BaseAstrology(1988)
+        m = a.get_mevas(2016)
+        print(u"#5 мева николаева на 2016: согме:%s люме:%s ванме:%s лунме:%s павме:%s "
+              % (m['sogme'], m['lume'], m['wangme'], m['lunme'], m['pawme']))
 
 if __name__ == '__main__':
     main()
